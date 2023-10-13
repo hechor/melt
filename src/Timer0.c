9a9dc02ce1b6728ff07090350443fdd850eabc6b
hechor <117984948+hechor@users.noreply.github.com>
2023-10-13 12:48:13 +0800

    Update Timer0.c

--- src/Timer0.c
+++ src/Timer0.c
@@ -1,14 +1,14 @@
 #include <REGX52.H>
 
-void Timer0_Init(void)		//1??@11.0592MHz
+void Timer0_Init(void)		//11.0592MHz
 {
-	TMOD &= 0xF0;		//��ʱ��0ģʽѡ��
+	TMOD &= 0xF0;		//定时器0模式选择
 	TMOD |= 0x01;
-	TL0 = 0xFF;		//���ö�ʱ����ֵ
+	TL0 = 0xFF;		//设置定时器初值
 	TH0 = 0xFF;		
-	TF0 = 0;		//���������־
-	TR0 = 1;		//T0�����п���λ
-	ET0 = 1;	//�����ж�Դ
-	EA = 1;		//�����ж�
-	PT0 = 0;	//�����ж����ȼ�
+	TF0 = 0;		//计数溢出标志
+	TR0 = 1;		//T0的运行控制位
+	ET0 = 1;	//设置中断源
+	EA = 1;		//开启中断
+	PT0 = 0;	//设置中断优先级
 }
